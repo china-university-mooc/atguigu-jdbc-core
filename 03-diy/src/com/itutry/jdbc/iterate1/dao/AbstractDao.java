35d0f5a097e69440db012b907d9995928e9ebabd
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-08-08 12:57:03 +0800

    refactor: SqlStatement

--- 03-diy/src/com/itutry/jdbc/iterate1/dao/AbstractDao.java
+++ 03-diy/src/com/itutry/jdbc/iterate1/dao/AbstractDao.java
@@ -6,6 +6,7 @@ import com.itutry.jdbc.iterate1.dao.sql.DeleteByIdStatement;
 import com.itutry.jdbc.iterate1.dao.sql.GetAllStatement;
 import com.itutry.jdbc.iterate1.dao.sql.GetByIdStatement;
 import com.itutry.jdbc.iterate1.dao.sql.InsertStatement;
+import com.itutry.jdbc.iterate1.dao.sql.SqlStatement;
 import com.itutry.jdbc.iterate1.dao.sql.UpdateStatement;
 import com.itutry.jdbc.iterate1.util.BeanListResultSetHandler;
 import com.itutry.jdbc.iterate1.util.BeanResultSetHandler;
@@ -35,75 +36,71 @@ public abstract class AbstractDao<T> implements Dao<T> {
   }
 
   public int insert(Connection conn, T obj) {
-    InsertStatement stat = new InsertStatement(table, obj);
-    System.out.println(stat);
-    return update(conn, stat.getSql(), stat.getParams());
+    return update(conn, new InsertStatement(table, obj));
   }
 
   public int update(Connection conn, T obj) {
-    UpdateStatement stat = new UpdateStatement(table, obj);
-    System.out.println(stat.getSql());
-    return update(conn, stat.getSql(), stat.getParams());
+    return update(conn, new UpdateStatement(table, obj));
   }
 
   public int deleteById(Connection conn, Object id) {
-    DeleteByIdStatement stat = new DeleteByIdStatement(table, id);
-    System.out.println(stat.getSql());
-    return update(conn, stat.getSql(), stat.getParams());
+    return update(conn, new DeleteByIdStatement(table, id));
   }
 
   public T getById(Connection conn, Object id) {
-    GetByIdStatement stat = new GetByIdStatement(table, id);
-    System.out.println(stat.getSql());
-    return queryBean(conn, stat.getSql(), stat.getParams());
+    return queryBean(conn, new GetByIdStatement(table, id));
   }
 
   public List<T> getAll(Connection conn) {
-    GetAllStatement stat = new GetAllStatement(table);
-    System.out.println(stat.getSql());
-    return queryBeanList(conn, stat.getSql(), stat.getParams());
+    return queryBeanList(conn, new GetAllStatement(table));
   }
 
   public Long count(Connection conn) {
-    CountStatement stat = new CountStatement(table);
-    System.out.println(stat.getSql());
-    return queryValue(conn, stat.getSql(), stat.getParams());
+    return queryValue(conn, new CountStatement(table));
   }
 
-  protected int update(Connection conn, String sql, Object... args) {
+  protected int update(Connection conn, SqlStatement stat) {
     try {
-      return QueryUtils.update(conn, sql, args);
+      System.out.println(stat);
+      return QueryUtils.update(conn, stat.getSql(), stat.getParams());
     } catch (SQLException e) {
       JdbcUtils.quietlyHandleException(e);
     }
     return 0;
   }
 
-  protected T queryBean(Connection conn, String sql, Object... args) {
+  protected T queryBean(Connection conn, SqlStatement stat) {
     try {
+      System.out.println(stat.getSql());
       BeanResultSetHandler<T> handler = new BeanResultSetHandler<>(type);
-      return QueryUtils.query(conn, handler, sql, args);
+      return QueryUtils.query(conn, handler, stat.getSql(), stat.getParams());
     } catch (SQLException e) {
       JdbcUtils.quietlyHandleException(e);
     }
     return null;
   }
 
-  protected List<T> queryBeanList(Connection conn, String sql, Object... args) {
+  protected List<T> queryBeanList(Connection conn, SqlStatement stat) {
     try {
+      System.out.println(stat.getSql());
       BeanListResultSetHandler<T> handler = new BeanListResultSetHandler<>(type);
-      return QueryUtils.query(conn, handler, sql, args);
+      return QueryUtils.query(conn, handler, stat.getSql(), stat.getParams());
     } catch (SQLException e) {
       JdbcUtils.quietlyHandleException(e);
     }
     return Collections.emptyList();
   }
 
-  protected <E> E queryValue(Connection conn, String sql, Object... args) {
+  protected <E> E queryValue(Connection conn, SqlStatement stat) {
+    System.out.println(stat.getSql());
+    return queryValue(conn, stat.getSql(), stat.getParams());
+  }
+
+  protected <E> E queryValue(Connection conn, String sql, Object... params) {
     try {
       ScalarResultSetHandler handler = new ScalarResultSetHandler();
       @SuppressWarnings("unchecked")
-      E result = (E) QueryUtils.query(conn, handler, sql, args);
+      E result = (E) QueryUtils.query(conn, handler, sql, params);
       return result;
     } catch (SQLException e) {
       JdbcUtils.quietlyHandleException(e);
